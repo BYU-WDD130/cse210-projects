8021d8906189d5aff3874358c6aefe8f166bcfac
BYU-WDD130 <henriquezdery@gmail.com>
2025-11-04 18:29:18 -0500

    week1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is ypur first name?");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string last = Console.ReadLine();
+
+
+        Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,64 @@
 using System;
 
+using System.Collections.Generic;
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+
+        {
+            Console.Write("Select a list of numbers, (0 to Quit):");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+
+        foreach (int number in numbers)
+
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+
+        float average = 0;
+        if (numbers.Count > 0)
+        {
+
+            average = (float)sum / numbers.Count;
+        }
+
+        Console.WriteLine($"The average is: {average}");
+
+
+
+        int max = numbers[0];
+        {
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+
+                    max = number;
+                }
+            }
+
+            Console.WriteLine($"The max is: {max}");
+        }
     }
 }
