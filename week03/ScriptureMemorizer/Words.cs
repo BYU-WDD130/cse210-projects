7a926da315e5593debf943f4327e8757c837e4c2
BYU-WDD130 <henriquezdery@gmail.com>
2025-11-13 15:11:04 -0500

    scripture

---
+++ week03/ScriptureMemorizer/Words.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+        (
+            for (int i = 0;)
+        )
+    }
